c96a54c5ff5a7452cc4c99e400d753fb649c33de
Sarah <trowbrsa@gmail.com>
2017-11-18 16:58:05 -0800

    Added simple Image component placeholder

---
+++ src/components/image.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+// could possibly be a functional component as of now
+class Image extends Component {
+  render(){
+    return (
+      <div>Here is where image will go</div>
+    )
+  }
+}
+
+export default Image;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Image from './components/image';
 
-class App extends React.Component {
+class App extends Component {
   render(){
     return(
       <div>
         <h1>Howdy from React!</h1>
+        <Image />
       </div>
     )
   }
