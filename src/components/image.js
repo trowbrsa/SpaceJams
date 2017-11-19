905bf091cb189d02547e2eadabaea9462066934c
Sarah <trowbrsa@gmail.com>
2017-11-18 21:07:10 -0800

    successfully calling NASA api and inputting data to React component, need to investigate spread operators with webpack

--- src/components/image.js
+++ src/components/image.js
@@ -1,11 +1,64 @@
 import React, { Component } from 'react';
+require('dotenv').config();
 
+const API_KEY = process.env.NASA_KEY;
 
-// could possibly be a functional component as of now
 class Image extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      image: {
+              'copyright': 'blank',
+              'date': 'blank',
+              'explanation': 'blank',
+              'title': 'blank',
+              'url': 'blank'
+            }
+    };
+
+    function checkStatus(response) {
+      if (response.status >= 200 && response.status < 300) {
+        return response
+      } else {
+        var error = new Error(response.statusText)
+        error.response = response
+        throw error
+      }
+    }
+
+    function parseJSON(response) {
+      return response.json()
+    }
+
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+      .then(checkStatus)
+      .then(parseJSON)
+      .then((data) => {
+        // spread operator needs plugin to work
+        // let prevState = this.state.image;
+        // prevState['title'] = data.title;
+        // this.setState(prevState => ({
+        //   image: {
+        //     ...prevState,
+        //     date: data.date
+        //   };
+        // }));
+
+        let image = Object.assign({}, this.state.image);
+        image.date = data.date;
+        this.setState({image});
+        console.log(data)
+      }).catch(function(error) {
+        console.log('request failed', error)
+      })
+  }
+
   render(){
     return (
-      <div>Here is where image will go</div>
+      <div className="imageContainer">
+        {this.state.image.date}
+      </div>
     )
   }
 }
