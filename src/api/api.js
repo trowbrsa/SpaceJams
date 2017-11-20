e011c083942b37e1c16421591876e7a7d81af84d
Sarah Trowbridge <sarah.trowbridge@gettyimages.com>
2017-11-20 15:03:11 -0800

    Successfully calling Spotify API

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,13 @@
 const language = require('@google-cloud/language');
+// choose one http library
 const fetch = require('node-fetch');
+import axios from 'axios';
 const client = new language.LanguageServiceClient();
 require('dotenv').config();
 
 const NASA_API_KEY = process.env.NASA_KEY;
+const SPOTIFY_ID = process.env.SPOTIFY_ID;
+const SPOTIFY_SECRET = process.env.SPOTIFY_SECRET;
 
 // how do we get this file to be called when we hit our app
 function checkStatus(response) {
@@ -21,16 +25,17 @@ function parseJSON(response) {
 }
 
 function callAPI() {
-  fetch(`https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`)
-  .then(checkStatus)
-  .then(parseJSON)
-  .then((data) => {
-    console.log(data);
-    console.log("now calling NLP");
-    callNLPLibrary();
-  }).catch(function(error) {
-      console.log('request failed', error)
-    })
+  // fetch(`https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`)
+  // .then(checkStatus)
+  // .then(parseJSON)
+  // .then((data) => {
+  //   console.log(data);
+    //console.log("now calling NLP");
+    // callNLPLibrary();
+    callSpotifyApi();
+  // }).catch(function(error) {
+  //     console.log('request failed', error)
+  //   })
 }
 
 function callNLPLibrary() {
@@ -55,6 +60,46 @@ function callNLPLibrary() {
       console.error('ERROR:', err);
     });
 }
+
+function callSpotifyApi(){
+
+  let payload = SPOTIFY_ID + ":" + SPOTIFY_SECRET;
+
+  console.log("This is just the secrets", payload)
+
+  let encodedPayload = new Buffer(payload).toString("base64");
+
+  //let data = JSON.stringify("grant_type=client_credentials");
+  //console.log(data);
+  //onsole.log("this is the the authentication: ", authentication);
+
+  axios({
+    url: 'https://accounts.spotify.com/api/token',
+    method: 'post',
+    params: {
+      grant_type: 'client_credentials'
+    },
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': "Basic " + encodedPayload
+    },
+  })
+
+  // fetch('https://accounts.spotify.com/api/token', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Authorization': authentication,
+  //     'Content-Type' : 'application/x-www-form-urlencoded',
+  //   },
+  //   body: data
+  // })
+  .then((data) => {
+    console.log(data);
+  }).catch(function(error) {
+    console.log('request failed', error)
+  })
+}
+
 
 const main = function() {
   callAPI();
