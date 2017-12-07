c2bbe22b3e94c6453cb6a6d435375de2a391ef4a
Sarah Trowbridge <sarah.trowbridge@gettyimages.com>
2017-12-06 16:16:55 -0800

    Added functionality to be able to close the modal by clicking outside of it

--- src/components/informationModal.js
+++ src/components/informationModal.js
@@ -1,7 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import closeIcon from '../../public/ic_close_white_24dp_2x.png';
 
 class InformationModal extends React.Component {
+  constructor(){
+    super();
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleOutsideClick = this.handleOutsideClick.bind(this);
+
+    this.state = {
+      popupVisible: false
+    };
+  }
+
+  handleClick() {
+    if (!this.state.popupVisible) {
+      // attach/remove event handler
+      document.addEventListener('click', this.handleOutsideClick, false);
+    } else {
+      document.removeEventListener('click', this.handleOutsideClick, false);
+    }
+
+    this.setState(prevState => ({
+       popupVisible: !prevState.popupVisible,
+    }));
+  }
+
+  handleOutsideClick(e) {
+    // ignore clicks on the component itself
+    if (this.node.contains(e.target)) {
+      return;
+    }
+
+    this.handleClick();
+  }
+
   render() {
     if(!this.props.show) {
       return null;
@@ -18,25 +52,37 @@ class InformationModal extends React.Component {
     };
 
     const modalStyle = {
-      backgroundColor: '#fff',
+      backgroundColor: 'rgba(0,0,0,0.5)',
       borderRadius: 5,
       maxWidth: 500,
       minHeight: 300,
       margin: '0 auto',
       padding: 30,
+      position: 'relative',
+    };
+
+    const closeModal = {
+      position: 'absolute',
+      top: 5,
+      right: 5,
+      filter: 'invert(50%)',
     };
 
     return (
-      <div className="backdrop" style={backdropStyle}>
-        <div className="modal" style={modalStyle}>
+        <div
+          className="modal"
+          style={modalStyle}
+          ref={ node => {this.node = node; }}
+          // this event needs to be on the icon itself 
+          onClick={this.handleClick}
+        >
           {this.props.children}
-          <div className="footer">
-            <button onClick={this.props.onClose}>
-              Close
-            </button>
-          </div>
+            {this.state.popupVisible && (
+              <div className="popover">
+                I'm a popover!
+              </div>
+            )}
         </div>
-      </div>
     );
   }
 }
@@ -46,5 +92,7 @@ InformationModal.propTypes = {
   show: PropTypes.bool,
   children: PropTypes.node
 };
+
+
 
 export default InformationModal;
