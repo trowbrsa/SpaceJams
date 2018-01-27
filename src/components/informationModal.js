12603177687d58a19c67dba6770400045f26d1e8
Sarah <trowbrsa@gmail.com>
2018-01-27 15:43:55 -0800

    Moved styling to scss

--- src/components/informationModal.js
+++ src/components/informationModal.js
@@ -15,7 +15,7 @@ class InformationModal extends React.Component {
 
     this.state = {
       modalVisible: false,
-      isChecked: true
+      selectedOption: 'tab1'
     };
 
   }
@@ -38,93 +38,52 @@ class InformationModal extends React.Component {
     this.handleClick();
   }
 
-  handleChange(){
-    this.setState(prevState => ({
-      isChecked: !prevState.isChecked,
-    }))
+  handleChange(changeEvent){
+    this.setState({
+      selectedOption: changeEvent.target.value
+    })
   }
 
   render() {
-    const informationModalStyle = {
-      position: 'fixed',
-      bottom: '1px',
-      right: 0,
-    }
-
-    const backdropStyle = {
-      position: 'fixed',
-      top: 0,
-      bottom: 0,
-      left: 0,
-      right: 0,
-      backgroundColor: 'rgba(0,0,0,0.3)',
-      padding: 50,
-    };
-
-    const modalStyle = {
-      backgroundColor: 'rgba(0,0,0,0.5)',
-      borderRadius: 5,
-      maxWidth: 500,
-      minHeight: 300,
-      margin: '0 auto',
-      padding: 30,
-      position: 'relative',
-      color: 'white',
-      lineHeight: 1.5,
-      fontFamily: 'Arial',
-      letterSpacing: 1.7,
-    };
-
-    const closeModal = {
-      position: 'absolute',
-      top: 5,
-      right: 5,
-      filter: 'invert(50%)',
-    };
-
-    const popoverHeader = {
-      fontSize: 25,
-      paddingBottom: 0.6 + 'em',
-    }
-
     return (
       <div
         className="modal-wrapper"
-        ref={node => {this.node = node;}}
-      >
+        ref={node => {this.node = node;}}>
         <div>
-          <span className='informationModal' style={informationModalStyle}
+          <span className='informationIcon'
             onClick={this.handleClick}>
             <img src={icon} alt="more info" />
           </span>
           {this.state.modalVisible && (
-            <div
-              className="modal"
-              style={modalStyle}
-              >
-              <div className="popover">
-                <div>
-                  <h1>Space Jams</h1>
-                  <main>
-                   <input id="tab1" type="radio" name="tabs" onChange={this.handleChange} checked={this.state.isChecked}/>
+            <div className="modal">
+              <div>
+                <h1>Space Jams</h1>
+                <main>
+                  <input id="tab1" type="radio" value="tab1" onChange={this.handleChange} checked={this.state.selectedOption === 'tab1'} />
                    <label htmlFor="tab1">Content 1</label>
 
-                   <input id="tab2" type="radio" name="tabs" onChange={this.handleChange} checked={this.state.isChecked}/>
+                   <input id="tab2" type="radio" value="tab2" onChange={this.handleChange} checked={this.state.selectedOption === 'tab2'}/>
                    <label htmlFor="tab2">Content 2</label>
-                   <section id="content1">
-                     <p>
-                      Intriguing Info
-                     </p>
-                   </section>
 
-                   <section id="content2">
-                     <p>
-                       More intriguing info
-                     </p>
-                   </section>
-                 </main>
-               </div>
-              </div>
+                   <input id="tab3" type="radio" value="tab3" onChange={this.handleChange} checked={this.state.selectedOption === 'tab3'}/>
+                   <label htmlFor="tab2">Content 3</label>
+
+                   <input id="tab4" type="radio" value="tab4" onChange={this.handleChange} checked={this.state.selectedOption === 'tab4'}/>
+                   <label htmlFor="tab2">Content 4</label>
+
+                 <section id="content1">
+                   <p>
+                    Intriguing Info
+                   </p>
+                 </section>
+
+                 <section id="content2">
+                   <p>
+                     More intriguing info
+                   </p>
+                 </section>
+               </main>
+             </div>
             </div>
           )}
       </div>
