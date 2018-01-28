61ddceb0ac16dcbdff2031771d983a91183b07d3
Sarah <trowbrsa@gmail.com>
2018-01-28 14:11:30 -0800

    Cleaning up async code

--- src/api/api.js
+++ src/api/api.js
@@ -14,13 +14,18 @@ const SPOTIFY_SECRET = process.env.SPOTIFY_SECRET;
 
 let apiData = {};
 
-function callAPI() {
+const callAPI = function() {
   axios.get(`https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`)
   .then(response => {
+    if(response.status !== 200) {
+      const error = new Error("error Calling NASA");
+      throw(error);
+    }
     let data = response.data;
     if(data.media_type !== 'image'){
-      console.log("no song available today :(")
-      return;
+      // make boolean in dailyData
+      const error = new Error("No image available today");
+      throw(error);
     }
     apiData.image_data =
       {
@@ -32,15 +37,14 @@ function callAPI() {
         'url': data.url,
         'media_type': data.media_type
       };
-      return(data.title, data.explanation);
+    return(data.title, data.explanation);
   })
   .then((title, explanation) => {
     callNLPLibrary(title, explanation);
   })
-  .catch(error => {
-    console.log('request failed', error);
-  })
+  .catch(error => console.log(error));
 }
+
 
 function callNLPLibrary(title, explanation) {
   const text = title + explanation;
@@ -53,6 +57,9 @@ function callNLPLibrary(title, explanation) {
   client
     .analyzeEntities({document})
     .then(results => {
+      if(results.status !== 200){
+        throw("Error with Google NLP", status.message);
+      }
       const entities = results[0].entities;
       const numberOfEntityExamples = entities.length > 5 ? 5 : entities.length;
       for(let i = 0; i < numberOfEntityExamples; i++){
@@ -69,8 +76,19 @@ function callNLPLibrary(title, explanation) {
       callSpotifyApi(entity);
     })
     .catch(err => {
-      console.error('Error from Google: ', err);
+      console.error(err);
     });
+}
+
+// update
+function getSong(processedData, token){
+  return new Promise(function(resolve, reject) {
+    let songSearchTerm = processedData.name;
+    let song = '';
+    axios.get(`https://api.spotify.com/v1/search?q=${songSearchTerm}&type=track`, {
+      headers: { 'Authorization': 'Bearer ' + token }
+    })
+  })
 }
 
 function callSpotifyApi(processedData) {
@@ -90,53 +108,33 @@ function callSpotifyApi(processedData) {
   })
   .then(response => {
     let token = response.data.access_token;
-    let getSong = function(entity){
-      return new Promise(function(resolve) {
-        let songSearchTerm = entity.name;
-        let song = '';
-        axios.get(`https://api.spotify.com/v1/search?q=${songSearchTerm}&type=track`, {
-          headers: { 'Authorization': 'Bearer ' + token }
-        })
-        .then(response => {
-          if(response.data.tracks.items.length > 0){
-            if('album' in response.data.tracks.items[0]){
-              let trackInfo = response.data.tracks.items[0];
-              let name = trackInfo.name;
-              console.log("success! Track name from Spotify is", name);
-              let album = trackInfo.album.name;
-              let artist = trackInfo.artists[0].name;
-              let uri = trackInfo.uri;
-              apiData.track_data =
-                {
-                  'name': name,
-                  'album': album,
-                  'artist': artist,
-                  'uri': uri
-                }
-
-              jsonFile.writeFile(file, apiData);
-              return resolve();
-            }
-          }
-          // no song found, use default
-          // this isn't working
-          song = '3gdewACMIVMEWVbyb8O9sY';
-          apiData.track_data =
-            {
-              'name': 'Test',
-              'song': song
-            }
-          console.log("use default song!");
-          jsonFile.writeFile(file, apiData);
-          return reject(song);
-        })
-        .catch(error => {
-          console.log("Error with Spotify", error);
-        })
-      })
-    }
-    processedData.find(getSong);
+    getSong(processedData, token);
   })
+  .then(response => {
+    if(response.data.tracks.items.length > 0){
+      if('album' in response.data.tracks.items[0]){
+        let trackInfo = response.data.tracks.items[0];
+        let name = trackInfo.name;
+        console.log("success! Track name from Spotify is", name);
+        let album = trackInfo.album.name;
+        let artist = trackInfo.artists[0].name;
+        let uri = trackInfo.uri;
+        apiData.track_data =
+          {
+            'name': name,
+            'album': album,
+            'artist': artist,
+            'uri': uri
+          }
+        jsonFile.writeFile(file, apiData);
+        return resolve();
+      }
+    }
+  })
+  .catch(error => {
+    console.log("Error with Spotify", error);
+  })
+  processedData.find(getSong);
 }
 
 
