908ca6dc3cb0869b462d5fb5217f31a507805954
Sarah <trowbrsa@gmail.com>
2018-01-28 19:34:58 -0800

    Animations for first time visitor working

--- src/components/FirstTimeVisitorModal.js
+++ src/components/FirstTimeVisitorModal.js
@@ -4,7 +4,7 @@ import './FirstTimeVisitorModal.scss';
 const FirstTimeVisitorModal = () => {
 
   return(
-    <div className="firstModal">
+    <div className="fade-in firstModal">
       <h2>Welcome to Space Jamz!</h2>
       <p>This project pairs NASA's Astronomy Photo of The Day with a
       corresponding song from Spotify using natural language processing.
