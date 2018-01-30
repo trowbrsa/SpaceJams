769b574ccb7ef5cfd4d16e2a35c50261096d03fe
Sarah <trowbrsa@gmail.com>
2018-01-29 21:39:07 -0800

    Potential fix for centering firstTimeVisitorModal

--- src/components/FirstTimeVisitorModal/FirstTimeVisitorModal.js
+++ src/components/FirstTimeVisitorModal/FirstTimeVisitorModal.js
@@ -4,17 +4,19 @@ import './FirstTimeVisitorModal.scss';
 const FirstTimeVisitorModal = () => {
 
   return(
-    <div className="firstModal">
-      <h2>Welcome to Space Jamz!</h2>
-      <p>This project pairs NASA's Astronomy Photo of The Day with a
-      corresponding song from Spotify using natural language processing.
-      </p>
-      <p>Have a listen by checking out the play button on the lower
-      left-hand corner of the screen, and learn more about the project
-      by clicking the information icon on the lower left-hand corner
-      of the screen.
-      </p>
-      <p>Happy Jamming!</p>
+    <div className="parent">
+      <div className="firstModal">
+        <h2>Welcome to Space Jamz!</h2>
+        <p>This project pairs NASA's Astronomy Photo of The Day with a
+        corresponding song from Spotify using natural language processing.
+        </p>
+        <p>Have a listen by checking out the play button on the lower
+        left-hand corner of the screen, and learn more about the project
+        by clicking the information icon on the lower left-hand corner
+        of the screen.
+        </p>
+        <p>Happy Jamming!</p>
+      </div>
     </div>
   )
 }
