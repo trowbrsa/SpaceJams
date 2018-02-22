054ceca82c106192f24f029b8e4e41a3df83d134
Sarah <trowbrsa@gmail.com>
2018-02-21 20:34:55 -0800

    Small refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,11 @@
-import React, { Component } from 'react';
+import AppRoot from './AppRoot/AppRoot';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import FirstTimeVisitorModal from './FirstTimeVisitorModal/FirstTimeVisitorModal';
-import Image from './Image/Image';
-import InformationModal from './InformationModal/InformationModal';
-import Loading from './Loading/Loading';
-import Loaded from './Loaded/Loaded';
-import Track from './Track/Track';
-import data from '../../dailyData.json';
-import './image.scss';
 
-class App extends Component {
-  constructor(props){
-    super(props);
-
-    this.setLoadedBackground = this.setLoadedBackground.bind(this);
-
-    this.state = {
-      isLoading: true,
-    }
+const app = {
+  initialize() {
+    ReactDOM.render(<AppRoot/>, document.getElementById('root'));
   }
+};
 
-  setLoadedBackground(e){
-    e.preventDefault();
-    this.setState({
-      isLoading: false
-    })
-  }
-
-  render(){
-
-    const isLoading = this.state.isLoading;
-
-    return(
-      <div>
-        {isLoading && <Loading />}
-        <Loaded
-          setLoadedBackground = {this.setLoadedBackground}
-          isLoading = {this.state.isLoading}
-        />
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-export default App;
+app.initialize();
---
+++ src/components/AppRoot/AppRoot.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Image from '../Image/Image';
+import InformationModal from '../InformationModal/InformationModal';
+import Loading from '../Loading/Loading';
+import Loaded from '../Loaded/Loaded';
+import Track from '../Track/Track';
+import data from '../../../dailyData.json';
+
+class AppRoot extends Component {
+  constructor(props){
+    super(props);
+
+    this.setLoadedBackground = this.setLoadedBackground.bind(this);
+
+    this.state = {
+      isLoading: true,
+    }
+  }
+
+  setLoadedBackground(e){
+    e.preventDefault();
+    this.setState({
+      isLoading: false
+    })
+  }
+
+  render(){
+
+    const isLoading = this.state.isLoading;
+
+    return(
+      <div>
+        {isLoading && <Loading />}
+        <Loaded
+          setLoadedBackground = {this.setLoadedBackground}
+          isLoading = {this.state.isLoading}
+        />
+      </div>
+    );
+  }
+}
+
+export default AppRoot;
