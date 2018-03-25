83e907cb5ff88392ce4062cf42e526203bce07c4
Sarah <trowbrsa@gmail.com>
2018-03-24 21:27:34 -0700

    Added SpaceJamz font to loading screen

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import style from './loading.scss';
+import '../../assets/fonts/_nasa.scss';
 
 class Loading extends Component {
 
   render(){
     return (
       <div className="loading">
-        <div>SpaceJamz</div>
+        <h1 style={{ fontFamily: 'nasaFont' }}>Space Jamz</h1>
       </div>
     )
   }
