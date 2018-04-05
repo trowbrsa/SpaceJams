42a73f0e980815d2234cb89ebabef0d9324f0bb3
Sarah Trowbridge <sarah.trowbridge@gettyimages.com>
2018-04-05 09:12:15 -0700

    Removed unnecessary files

--- buildScripts/server.js
+++ buildScripts/server.js
@@ -2,30 +2,17 @@ import express from 'express';
 import path from 'path';
 import open from 'open';
 import {main} from '../src/api/api.js';
-import dotenv from 'dotenv';
-
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
 const compiler = webpack(config);
-dotenv.config();
-
-
 const app = express();
-<<<<<<< HEAD
-let port = 8000;
-
-console.log("this is the port: ", port);
-
-=======
->>>>>>> Added debugging
 
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
 }));
 
-<<<<<<< HEAD
 app.listen(port, function (error) {
   console.log("in listen function");
   if(error) {
@@ -33,19 +20,11 @@ app.listen(port, function (error) {
   }
 });
 
-=======
->>>>>>> Added debugging
 app.get('/', function (req, res) {
   console.log("in get function");
   main();
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.listen(8000, function (error) {
-  console.log("in listen function");
-  if(error) {
-    console.log(error);
-  }
-});
 
 
